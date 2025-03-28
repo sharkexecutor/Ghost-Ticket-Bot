736cf5ed635ecaefca806c9dd9eac3634c07faac
Getsaturn <karimxain1@gmail.com>
2025-03-28 11:02:14 +0200

    Update start.sh

--- Ghost Ticket Bot/start.sh
+++ Ghost Ticket Bot/start.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-# Start bot and web services
+# Activate virtual environment (optional)
+. /opt/venv/bin/activate
+
+# Start bot
 cd bot && python bot.py &
+
+# Start web server (if using FastAPI/Flask)
 cd ../web && uvicorn main:app --host 0.0.0.0 --port 8000
+
 chmod +x start.sh
